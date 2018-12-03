74ef0ce7465fb3855c9456c8e5bd592f0ee6a73b
WisdomWang <294814110@qq.com>
2018-12-03 16:44:00 +0800

    first commit

---
+++ CAPlayer/CAplayerView.swift
@@ -0,0 +1,21 @@
+//
+//  CAplayerView.swift
+//  CAPlayer
+//
+//  Created by Cary on 2018/11/29.
+//  Copyright © 2018 Cary. All rights reserved.
+//
+
+import UIKit
+
+class CAplayerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
